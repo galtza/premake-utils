1f96a8688a3631989171b2efa9b432e17e9666ea
Raul Ramos <igaltza@gmail.com>
2023-04-30 18:01:35 +0200

    Restored wrongly removed function

--- premake-utils-dropbox.lua
+++ premake-utils-dropbox.lua
@@ -8,35 +8,28 @@
 -- Prevent Dropbox from synchronising specified folders
 -- ====================================================
 
-function exclude_files_from_dropbox(_files)
-
+function exclude_folders_from_dropbox(_folders)
     local script = ""
-
     if os.target() == "windows" then
-        for _, file in ipairs(_files) do
-            print(string.format("Excluding \"%s\" from Dropbox sync...", file))
+        for _, folder in ipairs(_folders) do
+            print(string.format("Excluding \"%s\" from Dropbox sync...", folder))
             script = script .. string.format([[
-                if (Test-Path "%s" -PathType Leaf) {
-                    Set-Content -Path "%s" -Stream com.dropbox.ignored -Value 1 | Out-Null
-                }
-            ]], file, file)
+                New-Item %s -type directory -force | Out-Null
+                Set-Content -Path '%s' -Stream com.dropbox.ignored -Value 1 | Out-Null
+            ]], folder, folder)
         end
-
         -- Feed the script to powershell process stdin
         local pipe = io.popen("powershell -command -", "w")
         pipe:write(script)
         pipe:close()
-
     elseif os.target() == "macosx" then
-        for _, file in ipairs(_files) do
-            print(string.format("Excluding \"%s\" from Dropbox sync...", file))
+        for _, folder in ipairs(_folders) do
+            print(string.format("Excluding \"%s\" from Dropbox sync...", folder))
             script = script .. string.format([[
-                if [ -f "%s" ]; then
-                    xattr -w com.dropbox.ignored 1 "%s"
-                fi
-            ]], file, file)
+                mkdir -p "%s"
+                xattr -w com.dropbox.ignored 1 "%s"
+            ]], folder, folder)
         end
-
         -- Run the script
         local pipe = io.popen("bash", "w")
         pipe:write(script)
