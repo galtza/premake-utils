7c0fb79a0bc1c8942e19846b32222794af0895ca
Raul Ramos <igaltza@gmail.com>
2023-04-30 13:04:10 +0200

    Separate functionality by folder/file

--- premake-utils-dropbox.lua
+++ premake-utils-dropbox.lua
@@ -7,48 +7,83 @@
 -- =========================================================
 -- Prevent Dropbox from synchronising some files and folders
 -- =========================================================
+--
+function remove_leading_space(_text)
+    local lines = {}
+    for line in _text:gmatch("[^\n]+") do
+        local cleanedLine = line:gsub("^%s*", "")
+        table.insert(lines, cleanedLine)
+    end
+    return table.concat(lines, "\n")
+end
 
-function exclude_from_dropbox(_items)
-
-    print("[] Excluding files and folders from Dropbox sync...")
-
+function exclude_folders_from_dropbox(_folders)
     local script = ""
 
     if os.target() == "windows" then
+        for _, folder in ipairs(_folders) do
+            print(string.format("[] Excluding \"%s\" from Dropbox sync...", folder))
+            script = script .. remove_leading_space(string.format([[
+                New-Item %s -type directory -force | Out-Null
+                Set-Content -Path '%s' -Stream com.dropbox.ignored -Value 1 | Out-Null
+            ]], folder, folder))
 
-        -- Do not allow Dropbox to sync these temporary files and folders
-
-        for _, item in ipairs(_items) do
-            script = script .. string.format([[
-                if (Test-Path "%s") {
-                    Set-Content -Path "%s" -Stream com.dropbox.ignored -Value 1
-                }
-            ]], item, item)
         end
 
         -- Feed the script to powershell process stdin
-
         local pipe = io.popen("powershell -command -", "w")
         pipe:write(script)
         pipe:close()
 
     elseif os.target() == "macosx" then
-
-        -- Do not allow Dropbox to sync these temporary files and folders
-
-        for _, item in ipairs(_items) do
-            script = script .. string.format([[
-                if [ -d "%s" ] || [ -f "%s" ]; then
-                    xattr -w com.dropbox.ignored 1 "%s"
-                fi
-            ]], item, item, item)
+        for _, folder in ipairs(_folders) do
+            print(string.format("[] Excluding \"%s\" from Dropbox sync...", folder))
+            script = script .. remove_leading_space(string.format([[
+                mkdir -p "%s"
+                xattr -w com.dropbox.ignored 1 "%s"
+            ]], folder, folder))
         end
+        script = remove_leading_space(script)
 
         -- Run the script
-
         local pipe = io.popen("bash", "w")
         pipe:write(script)
         pipe:close()
+    end
+end
 
+function exclude_files_from_dropbox(_files)
+
+    local script = ""
+
+    if os.target() == "windows" then
+        for _, file in ipairs(_files) do
+            print(string.format("[] Excluding \"%s\" from Dropbox sync...", file))
+            script = script .. remove_leading_space(string.format([[
+                if (Test-Path "%s" -PathType Leaf) {
+                    Set-Content -Path "%s" -Stream com.dropbox.ignored -Value 1
+                }
+            ]], file, file))
+        end
+
+        -- Feed the script to powershell process stdin
+        local pipe = io.popen("powershell -command -", "w")
+        pipe:write(script)
+        pipe:close()
+
+    elseif os.target() == "macosx" then
+        for _, file in ipairs(_files) do
+            print(string.format("[] Excluding \"%s\" from Dropbox sync...", file))
+            script = script .. remove_leading_space(string.format([[
+                if [ -f "%s" ]; then
+                    xattr -w com.dropbox.ignored 1 "%s"
+                fi
+            ]], file, file))
+        end
+
+        -- Run the script
+        local pipe = io.popen("bash", "w")
+        pipe:write(script)
+        pipe:close()
     end
 end
