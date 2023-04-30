4ef433e2112a04c885b0e82b858f3a0235100604
Raul Ramos <igaltza@gmail.com>
2023-04-30 17:59:01 +0200

    Remove unused function

--- premake-utils-dropbox.lua
+++ premake-utils-dropbox.lua
@@ -4,43 +4,9 @@
     See the LICENSE file in the project root for more information.
 --]]
 
--- =========================================================
--- Prevent Dropbox from synchronising some files and folders
--- =========================================================
-
-function exclude_folders_from_dropbox(_folders)
-    local script = ""
-
-    if os.target() == "windows" then
-        for _, folder in ipairs(_folders) do
-            print(string.format("Excluding \"%s\" from Dropbox sync...", folder))
-            script = script .. string.format([[
-                New-Item %s -type directory -force | Out-Null
-                Set-Content -Path '%s' -Stream com.dropbox.ignored -Value 1 | Out-Null
-            ]], folder, folder)
-
-        end
-
-        -- Feed the script to powershell process stdin
-        local pipe = io.popen("powershell -command -", "w")
-        pipe:write(script)
-        pipe:close()
-
-    elseif os.target() == "macosx" then
-        for _, folder in ipairs(_folders) do
-            print(string.format("Excluding \"%s\" from Dropbox sync...", folder))
-            script = script .. string.format([[
-                mkdir -p "%s"
-                xattr -w com.dropbox.ignored 1 "%s"
-            ]], folder, folder)
-        end
-
-        -- Run the script
-        local pipe = io.popen("bash", "w")
-        pipe:write(script)
-        pipe:close()
-    end
-end
+-- ====================================================
+-- Prevent Dropbox from synchronising specified folders
+-- ====================================================
 
 function exclude_files_from_dropbox(_files)
 
